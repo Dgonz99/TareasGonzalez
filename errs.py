5345fe0cd7b329484f908fd3eb8e929817fcf905
Dgonz99 <davgonzalez@estudiantec.cr>
2026-02-27 06:22:50 -0600

    Update errs.py
    Se actualizo errs.py para resolver todos los errores segun flake8

--- errs.py
+++ errs.py
@@ -1,18 +1,19 @@
-
 def printFunc():
-	#//This is a comment omg so exciting
+    # //This is a comment omg so exciting
 
-	lis=["This","list","has","no","whitespaces","between","elements"]
+    lis = ["This", "code", "has", "no", "personality", "at", "all"]
 
-	print("But lets be real, Who cares??")
+    print("But its readable i guess")
 
-	string="Jesus christ this code is indented with tabs, someone call the police!"
-	q=len(string)
-	for i in range(0,50):
-		q+=len(lis)
+    string = """This code was indented with
+    tabs, INGSOC fixed it"""
+    q = len(string)
+    for i in range(0, 50):
+        q += len(lis)
 
-var="Oh no im directly comparing bools instead of using 'is'! The horror!"
-if isinstance(var,str)==True:
-	printFunc()
 
-print("Hell i might not end my code in a newline just for the giggles")
+var = "Ive been sent to a re-education camp for comparing bools directly"
+if isinstance(var, str) is True:
+    printFunc()
+
+print("Big brother 'asked' me to end this code with a newline")
