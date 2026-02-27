94533eac14bfb306d7aab775d6165613b3e335d6
Dgonz99 <davgonzalez@estudiantec.cr>
2026-02-27 06:14:06 -0600

    Added file with flake8 errors
    Se agrega un archivo con mutiples errores segun flake8

---
+++ errs.py
@@ -0,0 +1,18 @@
+
+def printFunc():
+	#//This is a comment omg so exciting
+
+	lis=["This","list","has","no","whitespaces","between","elements"]
+
+	print("But lets be real, Who cares??")
+
+	string="Jesus christ this code is indented with tabs, someone call the police!"
+	q=len(string)
+	for i in range(0,50):
+		q+=len(lis)
+
+var="Oh no im directly comparing bools instead of using 'is'! The horror!"
+if isinstance(var,str)==True:
+	printFunc()
+
+print("Hell i might not end my code in a newline just for the giggles")
